dce04dfc3f9f1b64da7e406e531e7cc72b1101ab
Kenneth <20908694+tinyKnightOfCoding@users.noreply.github.com>
2025-02-05 15:23:17 +0100

    install analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Quicksand } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={quicksand.className}>{children}</body>
+      <body className={quicksand.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
